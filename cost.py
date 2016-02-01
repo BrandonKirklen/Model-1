f04eccb29fd24e5c6d9f2422e6283e275cea05b2
Brandon Kirklen <brandonkirklen@gmail.com>
2016-02-01 00:58:19 -0800

    Added two method support

--- cost.py
+++ cost.py
@@ -1,9 +1,16 @@
 import riskEvaluation as risk
 
 
-def ROI(orbitalObjects, method):
-    baseCost = 10000000000
-    reduction = risk.reduction(orbitalObjects)
-    investment = method.cost
-    gain = baseCost * reduction
-    return (gain - investment)/investment
+def ROI(orbitalObjects, method, method2=None):
+    if method2 is not None:
+        baseCost = 10000000000
+        reduction = risk.reduction(orbitalObjects)
+        investment = method.cost + method2.cost
+        gain = baseCost * reduction
+        return (gain - investment)/investment
+    else:
+        baseCost = 10000000000
+        reduction = risk.reduction(orbitalObjects)
+        investment = method.cost
+        gain = baseCost * reduction
+        return (gain - investment)/investment
