3a40fbc993c4c171246702210cfd0b8652bb9b76
Brandon Kirklen <brandonkirklen@gmail.com>
2016-02-01 17:04:56 -0800

    Modified Cost Functions with extra probability of crash functionality

--- cost.py
+++ cost.py
@@ -1,16 +1,17 @@
 import riskEvaluation as risk
+probabilityOfCrashDefault = 1/221.
 
 
-def ROI(orbitalObjects, method, method2=None):
+def ROI(orbitalObjects, method, method2=None, probabilityOfCrash=probabilityOfCrashDefault):
     if method2 is not None:
         baseCost = 10000000000
-        reduction = risk.reduction(orbitalObjects)
+        reduction = risk.reduction(orbitalObjects, probabilityOfCrash)
         investment = method.cost + method2.cost
         gain = baseCost * reduction
         return (gain - investment)/investment
     else:
         baseCost = 10000000000
-        reduction = risk.reduction(orbitalObjects)
+        reduction = risk.reduction(orbitalObjects, probabilityOfCrash)
         investment = method.cost
         gain = baseCost * reduction
         return (gain - investment)/investment
